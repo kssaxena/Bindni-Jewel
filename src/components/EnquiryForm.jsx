732ddb2073692fd6be36dc058cb4ee976dc423ab
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2025-07-10 19:24:39 +0530

    feat: enhance ContactUs page with enquiry form and responsive contact cards layout

--- src/components/EnquiryForm.jsx
+++ src/components/EnquiryForm.jsx
@@ -24,86 +24,94 @@ export default function EnquiryForm() {
   };
 
   return (
-    <div className="max-w-2xl mx-auto bg-white shadow-xl rounded-2xl p-8 mt-10">
+    <div className="max-w-2xl mx-auto bg-white/90 shadow-xl rounded-2xl p-8 mt-10">
       <h2 className="text-2xl mb-6 text-center text-gray-800">Enquiry Form</h2>
       <form onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <label className="block text-gray-700">Full Name</label>
-          <input
-            type="text"
-            name="fullName"
-            onChange={handleChange}
-            required
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none"
-          />
+        <div className="flex flex-col lg:flex-row justify-evenly items-center gap-2 w-full">
+          <div className="lg:w-1/2 w-full">
+            <label className="block text-gray-700">Full Name</label>
+            <input
+              type="text"
+              name="fullName"
+              onChange={handleChange}
+              required
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none"
+            />
+          </div>
+
+          <div className="lg:w-1/2 w-full">
+            <label className="block text-gray-700">Email</label>
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              required
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none"
+            />
+          </div>
         </div>
 
-        <div>
-          <label className="block text-gray-700">Email</label>
-          <input
-            type="email"
-            name="email"
-            onChange={handleChange}
-            required
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none"
-          />
+        <div className="flex flex-col lg:flex-row justify-evenly items-center gap-2 w-full">
+          <div className="lg:w-1/2 w-full">
+            <label className="block text-gray-700">Phone Number</label>
+            <input
+              type="tel"
+              name="phone"
+              onChange={handleChange}
+              required
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none"
+            />
+          </div>
+
+          <div className="lg:w-1/2 w-full">
+            <label className="block text-gray-700">City / Location</label>
+            <input
+              type="text"
+              name="city"
+              onChange={handleChange}
+              required
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none"
+            />
+          </div>
         </div>
 
-        <div>
-          <label className="block text-gray-700">Phone Number</label>
-          <input
-            type="tel"
-            name="phone"
-            onChange={handleChange}
-            required
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none"
-          />
-        </div>
+        <div className="flex flex-col lg:flex-row justify-evenly items-center gap-2 w-full">
+          <div className="lg:w-1/2 w-full">
+            <label className="block text-gray-700">Interested Model</label>
+            <select
+              name="businessModel"
+              onChange={handleChange}
+              required
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none"
+            >
+              <option value="">Select an option</option>
+              <option value="Kiosk">Kiosk</option>
+              <option value="Retail Store Jewel or Ethnic">
+                Retail Store Jewel or Ethnic
+              </option>
+              <option value="Retail Store - Jewelry + Ethnic Wear">
+                Retail Store – Jewelry + Ethnic Wear
+              </option>
+            </select>
+          </div>
 
-        <div>
-          <label className="block text-gray-700">City / Location</label>
-          <input
-            type="text"
-            name="city"
-            onChange={handleChange}
-            required
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none"
-          />
-        </div>
-
-        <div>
-          <label className="block text-gray-700">Interested Model</label>
-          <select
-            name="businessModel"
-            onChange={handleChange}
-            required
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none"
-          >
-            <option value="">Select an option</option>
-            <option value="Kiosk">Kiosk</option>
-            <option value="Retail Store Jewel or Ethnic">
-              Retail Store Jewel or Ethnic
-            </option>
-            <option value="Retail Store - Jewelry + Ethnic Wear">
-              Retail Store – Jewelry + Ethnic Wear
-            </option>
-          </select>
-        </div>
-
-        <div>
-          <label className="block text-gray-700">Investment Range (in ₹)</label>
-          <select
-            name="investmentRange"
-            onChange={handleChange}
-            required
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none"
-          >
-            <option value="">Select an option</option>
-            <option value="3-5 Lakh">₹3–5 Lakh</option>
-            <option value="5–9 Lakh">₹5–9 Lakh</option>
-            <option value="9–12 Lakh">₹9–12 Lakh</option>
-            <option value="Above 12 Lakh">Above ₹12 Lakh</option>
-          </select>
+          <div className="lg:w-1/2 w-full">
+            <label className="block text-gray-700">
+              Investment Range (in ₹)
+            </label>
+            <select
+              name="investmentRange"
+              onChange={handleChange}
+              required
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none"
+            >
+              <option value="">Select an option</option>
+              <option value="3-5 Lakh">₹3–5 Lakh</option>
+              <option value="5–9 Lakh">₹5–9 Lakh</option>
+              <option value="9–12 Lakh">₹9–12 Lakh</option>
+              <option value="Above 12 Lakh">Above ₹12 Lakh</option>
+            </select>
+          </div>
         </div>
 
         <div>
--- src/pages/About/ContactUs.jsx
+++ src/pages/About/ContactUs.jsx
@@ -1,12 +1,88 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { IoMdCall } from "react-icons/io";
+import { SiGmail } from "react-icons/si";
+import { IoLocationSharp } from "react-icons/io5";
+import EnquiryForm from "../../components/EnquiryForm";
 
 const ContactUs = () => {
+  const cardData = [
+    {
+      id: 1,
+      icon: <IoLocationSharp />,
+      label: "Reach Us",
+      description: (
+        <h1>
+          A18 R V Tower, Nitya nand Marg, <br /> Vaishali Nagar, Jaipur date 27
+          / 6
+        </h1>
+      ),
+      link: "https://maps.app.goo.gl/CZJRh2oK7hAxGvdj7",
+      link_name: "Location",
+      iconClassname: "text-rose-700",
+    },
+    {
+      id: 2,
+      icon: <IoMdCall />,
+      label: "Contact Us",
+      description: (
+        <h1>
+          Have questions about our products <br /> or want help placing an
+          order?
+        </h1>
+      ),
+      link: "tel:+9931430899",
+      link_name: "Call",
+      iconClassname: "text-blue-700",
+    },
+    {
+      id: 3,
+      icon: <SiGmail />,
+      label: "Write Us",
+      description: (
+        <h1>
+          For business queries, <br /> order updates, or feedback,
+        </h1>
+      ),
+      link: "mailto:bindnijewel@gmail.com",
+      link_name: "Mail",
+      iconClassname: "text-red-700",
+    },
+  ];
+  const ContactCards = ({
+    id,
+    icon,
+    label,
+    link,
+    link_name,
+    description,
+    iconClassname,
+  }) => {
+    return (
+      <div
+        key={id}
+        className="flex lg:justify-center justify-evenly items-center lg:flex-col flex-row bg-white lg:w-96 w-full lg:gap-10 gap-5 p-5 shadow-2xl lg:h-96"
+      >
+        <h1 className={`text-5xl ${iconClassname}`}>{icon}</h1>
+        <h1 className="text-4xl cinzel-font font-medium hidden lg:flex">
+          {" "}
+          {label}{" "}
+        </h1>
+        <p className="hidden lg:flex w-full text-center flex justify-center items-center">
+          {description}
+        </p>
+        <a
+          target="_blank"
+          href={link}
+          className="border-b duration-200 ease-in-out megrim-font font-extrabold text-2xl cursor-pointer hover:px-5 "
+        >
+          {link_name}
+        </a>
+      </div>
+    );
+  };
   return (
     <div>
-      {/* <h1 className="text-black text-outline-white bg-black">
-        Outlined Text
-      </h1>{" "} */}
       <div className="flex lg:h-fit relative">
         <motion.div
           whileInView={{ opacity: 1, y: 0 }}
@@ -17,7 +93,7 @@ const ContactUs = () => {
           <img
             className="h-[50vh] lg:h-full w-full object-cover object-center"
             src={
-              "https://ik.imagekit.io/jarvisai/Ariser%20Tradco/A%20high-resolution,%20realistic%20daytime%20photograph%20of%20a%20house%20exterior%20with%20a%20symmetrical%20composition.%20In%20the%20center,%20there%20is%20a%20modern%20dark%20brown%20wooden%20double%20door%20with%20detailed%20paneling%20and%20a%20brass%20handle.%20To%20the%20right%20of%20the%20door%20is%20a%20%20(2).jpg"
+              "https://ik.imagekit.io/pz8qfunss/Home/generate%20an%20image%20,which%20should%20include%20indian%20handbags,%20handicrafts,%20blankets%20all%20in%20one.jpg?updatedAt=1751958776805"
             }
           />
         </motion.div>
@@ -28,13 +104,30 @@ const ContactUs = () => {
             transition={{ duration: 1.5 }}
             className=" h-full drop-shadow-2xl w-full flex justify-center items-center bg-black/60 text-[#DEDFD8] cinzel-font tracking-widest flex-col lg:gap-20"
           >
-            <span className="text-3xl">
+            <span className="text-3xl flex flex-col lg:flex-row justify-center items-center gap-5">
               Bindni{" "}
               <span className="text-lg">A house of bridal collections</span>
             </span>
             <span className="lg:text-7xl text-3xl">Contact Us</span>
           </motion.h1>
         </div>
+      </div>
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-5 py-10">
+        {cardData.map((item) => (
+          <ContactCards
+            key={item.id}
+            id={item.id}
+            icon={item.icon}
+            label={item.label}
+            link={item.link}
+            link_name={item.link_name}
+            description={item.description}
+            iconClassname={item.iconClassname}
+          />
+        ))}
+      </div>
+      <div className="w-full lg:py-10 pb-5">
+        <EnquiryForm />
       </div>
     </div>
   );
